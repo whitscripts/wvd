0e710c80aed864681c7eba9358902c09791e4d4d
Adam Whitlatch <44882392+whitscripts@users.noreply.github.com>
2020-03-05 16:27:48 -0700

    Add files via upload

---
+++ Add Users to HP Groups in bulk.ps1
@@ -0,0 +1,12 @@
+﻿Connect-AzAccount   #log into Azure
+Add-RDSAccount -DeploymentUrl https://rdbroker.wvd.microsoft.com   # Log into WVD Tenant
+
+$tenant = "myWVDTenantName"   #your WVD Tenant Name
+$Hostpool = "MyHostPool"      #Security Group You would like to add
+$users = Get-azadgroupmember -GroupDisplayName <AAD Security Group Name> # Get User Names from the group
+
+#Add each user to the correct Host Pool Group
+foreach ($user in $users){
+
+Add-RdsAppGroupUser -TenantName $tenant -HostPoolName $Hostpool -AppGroupName “Desktop Application Group” -UserPrincipalName $user.UserPrincipalName
+}
